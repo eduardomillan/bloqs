6dfb1e767bc058dc17d812138e84e4d529ef40b0
Jesús Flores <jesus.flores@bq.com>
2015-03-30 11:10:38 +0200

    Basic bloq resource file

---
+++ res/basic_bloqs.js
@@ -0,0 +1,45 @@
+/**
+ * Created by jesus on 30/03/15.
+ */
+
+var basic_bloqs = function(){
+
+    var data = {
+        setup: {
+            label: 'setup',
+            down: true,
+            color: '#000',
+            code: {setup: "", loop: "void setup (){\n"}
+        },
+        loop: {
+            label: 'loop',
+            down: true,
+            color: '#000',
+            code: {setup: "", loop: "void loop (){\n"}
+        },
+        horizontal3Inputs: {
+            up: true,
+            down: true,
+            path: '',
+            color: '#e2e2e2',
+            text : [ ["int:", {input : 'bloqInput',type:"int",label:"INPUT"}, "number:", {input : 'bloqInput',type:"int",label:"INPUT"}, "userInput", {input:'userInput', type:"string",label:"userInput"}] ] ,
+            code: {setup:"trial({0},{1},{2});\n", loop:"trial({0},{1},{2});\n"}
+        },
+        basicInputDropdown: {
+            output: 'int',
+            color: '#e2e2e2',
+            text:[[{input:'dropdown', type:"text",data:[{label:'ON',value:'HIGH'},{label:'OFF',value:'LOW'}]}]],
+            code: {setup:"{0}", loop:"{0}"}
+        },
+        basicInputNumber: {
+            output: 'int',
+            color: '#e2e2e2',
+            text:[["number",{input:'userInput', type:"number",label:"number"}]],
+            code: {setup:'{0}', loop:'{0}'}
+        }
+    };
+
+    var getBloqs = function(){
+        return JSON.stringify(data);
+    }
+};
