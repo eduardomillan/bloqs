2c7af7c0cc84c756d578dc5a3f1cc856de2bb972
Tom Calvo <calvo.tom@gmail.com>
2015-04-20 09:57:23 +0200

    Archive in folder old, svg develop, and change prototypes to plain json in blows definition
    Change-Id: I8957b6bb0c07f44cbb10d71ee2f5b37c8aaf34a2

--- src/scripts/bloqs/setVariableBloq.js
+++ src/scripts/bloqs/setVariableBloq.js
@@ -7,21 +7,20 @@ define(
         'bloqs/statementBloq'
     ],
     function(_, StatementBloq) {
-        var SetVariableBloq = Object.create(StatementBloq);
-        SetVariableBloq.name = 'setVariable';
-        SetVariableBloq.content = [
-            [{
-                alias: 'stringInput',
-                placeholder: 'Introduce el nombre de la variable'
-            }, {
-                alias: 'text',
-                value: '='
-            }, {
-                alias: 'bloqInput',
-                acceptType: 'all'
-            }]
-        ];
-
-        return SetVariableBloq;
+        return _.merge(Object.create(StatementBloq), {
+            name: 'setVariable',
+            content: [
+                [{
+                    alias: 'stringInput',
+                    placeholder: 'Introduce el nombre de la variable'
+                }, {
+                    alias: 'text',
+                    value: '='
+                }, {
+                    alias: 'bloqInput',
+                    acceptType: 'all'
+                }]
+            ]
+        });
     }
 );
--- src/scripts/bloqs/statementBloq.js
+++ src/scripts/bloqs/statementBloq.js
@@ -1,17 +1,17 @@
 /*global define */
 'use strict';
 
-define(['lodash'], function(_) {
-    var StatementBLoq = function() {};
-    StatementBLoq.prototype.type = 'statement';
-    StatementBLoq.prototype.connectors = [{
-        type: 'connector-top',
-        accept: ['connector-bottom']
+define(function() {
+    return {
+        type: 'statement',
+        connectors: [{
+            type: 'connector-top',
+            accept: ['connector-bottom']
 
-    }, {
-        type: 'connector-bottom',
-        accept: ['connector-top']
+        }, {
+            type: 'connector-bottom',
+            accept: ['connector-top']
 
-    }];
-    return new StatementBLoq();
+        }]
+    };
 });
