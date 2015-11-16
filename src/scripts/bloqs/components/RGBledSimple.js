d6a5544ec932b76dfe2780a11e8bda3e3a49e757
Irene Sanz <irene.sanz@bq.com>
2015-11-16 15:26:47 +0100

    Added new colours to RGBLedSimple and added zowigestures
    Change-Id: I7fc895813b115b01e65632fd4a83322d049d2d14

--- src/scripts/bloqs/components/RGBledSimple.js
+++ src/scripts/bloqs/components/RGBledSimple.js
@@ -24,16 +24,34 @@ var bloq = _.merge(_.clone(StatementBloq, true), {
             id: 'COLOR',
             alias: 'staticDropdown',
             //'Raíz cuadrada', 'Valor absoluto', '-', 'ln', 'log10', 'e^', '10^']
-            options: [{
+            options: [ {
+                label: 'bloq-rgbLed-simple-white',
+                value: '255,255,255'
+            }, {
+                label: 'bloq-rgbLed-simple-yellow',
+                value: '255,255,0'
+            }, {
+                label: 'bloq-rgbLed-simple-orange',
+                value: '200,50,0'
+            }, {
                 label: 'bloq-rgbLed-simple-red',
                 value: '255,0,0'
             }, {
                 label: 'bloq-rgbLed-simple-green',
                 value: '0,255,0'
             }, {
+                label: 'bloq-rgbLed-simple-dark-green',
+                value: '0,60,102'
+            }, {
                 label: 'bloq-rgbLed-simple-blue',
+                value: '40,40,255'
+            }, {
+                label: 'bloq-rgbLed-simple-dark-blue',
                 value: '0,0,255'
-            }, ]
+            }, {
+                label: 'bloq-rgbLed-simple-pink',
+                value: '255,0,255'
+            }]
         }]
     ],
     code: '{LED}.setRGBcolor({COLOR});'
