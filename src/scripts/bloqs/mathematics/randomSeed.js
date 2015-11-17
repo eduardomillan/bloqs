a21a4c7511ba9d4858bd3899e886fcb4671d4711
Irene Sanz <irene.sanz@bq.com>
2015-11-17 14:44:27 +0100

    Added random seed block
    Change-Id: I693e12975766c471d5ae2349e09d8bf7a9e27c0f

---
+++ src/scripts/bloqs/mathematics/randomSeed.js
@@ -0,0 +1,27 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../../utils'),
+    StatementBloq = require('./../statementBloq');
+
+var bloq = _.merge(_.clone(StatementBloq, true), {
+
+    name: 'randomSeed',
+    bloqClass: 'bloq-random-seed',
+    content: [
+        [{
+            alias: 'text',
+            value: 'bloq-random-seed'
+        }]
+    ],
+    code: 'randomSeed(analogRead(0));',
+    returnType: {
+        type: 'simple',
+        value: 'float'
+    }
+});
+
+utils.generateBloqInputConnectors(bloq);
+
+module.exports = bloq;
