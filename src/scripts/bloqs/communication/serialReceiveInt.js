70dbb220f8ea35766ac4037f64a5098ea9557cf7
Irene Sanz <irene.sanz@bq.com>
2016-04-18 12:49:01 +0200

    Added new serialReceiveInt bloq, with its literals
    Change-Id: I146431c34dc900f8c25f48bf35586f1417c63990

---
+++ src/scripts/bloqs/communication/serialReceiveInt.js
@@ -0,0 +1,40 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../build-utils'),
+    OutputBloq = require('./../outputBloq');
+
+/**
+ * Bloq name: serialReceive
+ *
+ * Bloq type: Output
+ *
+ * Description: It returns what is received by the serial port.
+ *
+ * Return type: String
+ */
+
+var serialReceiveInt = _.merge(_.clone(OutputBloq, true), {
+
+    name: 'serialReceiveInt',
+    bloqClass: 'bloq-serial-receive-number',
+    content: [
+        [{
+            id: 'SERIAL',
+            alias: 'dynamicDropdown',
+            options: 'serialElements'
+        }, {
+            alias: 'text',
+            value: 'bloq-serial-receiver-receive-number'
+        }]
+    ],
+    code: '{SERIAL}.readInt()',
+    returnType: {
+        type: 'simple',
+        value: 'int'
+    }
+});
+utils.generateBloqInputConnectors(serialReceiveInt);
+
+module.exports = serialReceiveInt;
