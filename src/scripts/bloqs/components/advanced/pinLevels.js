c90203a5957e59395f70c52996805ba7ab174252
Tom Calvo <calvo.tom@gmail.com>
2016-07-04 10:16:45 +0200

    new bloq pinLevels

---
+++ src/scripts/bloqs/components/advanced/pinLevels.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../../build-utils'),
+    OutputBloq = require('./../../outputBloq');
+
+/**
+ * Bloq name: pin level
+ *
+ * Bloq type: Output
+ *
+ * Description: It returns the selected pin level constant (HIGH or LOW).
+ *
+ * Return type: float
+ */
+
+var pinLevels = _.merge(_.clone(OutputBloq, true), {
+
+    name: 'pinLevels',
+    bloqClass: 'bloq-pinlevels',
+    content: [
+        [{
+            id: 'STATE',
+            alias: 'staticDropdown',
+            options: [{
+                label: 'bloq-pinlevel-high',
+                value: 'HIGH'
+            }, {
+                label: 'bloq-pinlevel-low',
+                value: 'LOW'
+            }]
+        }]
+    ],
+    code: '{STATE}',
+    returnType: {
+        type: 'simple',
+        value: 'bool'
+    }
+});
+
+utils.generateBloqInputConnectors(pinLevels);
+
+module.exports = pinLevels;
