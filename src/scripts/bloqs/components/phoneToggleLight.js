a8f7888c7af2cab34d8202abbb39db63778532f2
alvarosBQ <johndoe@example.com>
2016-11-28 15:01:47 +0100

    Added toggle torch bloq

---
+++ src/scripts/bloqs/components/phoneToggleLight.js
@@ -0,0 +1,50 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../build-utils'),
+    StatementBloq = require('./../statementBloq');
+
+/**
+ * Bloq name: phoneSendText
+ *
+ * Bloq type: Statement
+ *
+ * Description: It sends a message to mobile by bluetooth
+ *
+ * Return type: none
+ */
+
+var phoneToggleLight = _.merge(_.clone(StatementBloq, true), {
+
+    name: 'phoneSendText',
+    bloqClass: 'bloq-phone-toggle',
+    content: [
+        [{
+            id: 'PHONE',
+            alias: 'dynamicDropdown',
+            options: 'phoneElements'
+        }, {
+            alias: 'text',
+            value: 'bloq-phone-toggle-light'
+        }, {
+            bloqInputId: 'DATA',
+            alias: 'bloqInput',
+            acceptType: ['all'],
+            suggestedBloqs: ['number', 'selectVariable']
+        },  {
+            alias: 'text',
+            value: 'bloq-phone-toggle-units'
+        }]
+    ],
+    code: '{PHONE}.println("write-" + {DATA});',
+
+    arduino: {
+        includes: ['BitbloqSoftwareSerial.h'],
+        code: '{PHONE}.println(String("toggleFlashlight-")+String({DATA}));'
+    }
+
+});
+utils.generateBloqInputConnectors(phoneToggleLight);
+
+module.exports = phoneToggleLight;
