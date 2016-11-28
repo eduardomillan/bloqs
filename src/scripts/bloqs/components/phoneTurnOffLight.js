3cef1f7cfb8a1d4ec859e5de6fce2750b13330a4
alvarosBQ <johndoe@example.com>
2016-11-28 16:20:06 +0100

    Added phoneTurnOffLight bloq

---
+++ src/scripts/bloqs/components/phoneTurnOffLight.js
@@ -0,0 +1,42 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../build-utils'),
+    StatementBloq = require('./../statementBloq');
+
+/**
+ * Bloq name: phoneToggleLight
+ *
+ * Bloq type: Statement
+ *
+ * Description: It sends a message to mobile by bluetooth
+ *
+ * Return type: none
+ */
+
+var phoneTurnOffLight = _.merge(_.clone(StatementBloq, true), {
+
+    name: 'phoneTurnOffLight',
+    bloqClass: 'bloq-phone-turnoff-light',
+    content: [
+        [{
+            id: 'PHONE',
+            alias: 'dynamicDropdown',
+            options: 'phoneElements'
+        }, {
+            alias: 'text',
+            value: 'bloq-phone-turnoff-light'
+        }]
+    ],
+    code: '{PHONE}.println("turnonFlashlight-");',
+
+    arduino: {
+        includes: ['BitbloqSoftwareSerial.h'],
+        code: '{PHONE}.println(String("turnoffFlashlight-"));'
+    }
+
+});
+utils.generateBloqInputConnectors(phoneTurnOffLight);
+
+module.exports = phoneTurnOffLight;
