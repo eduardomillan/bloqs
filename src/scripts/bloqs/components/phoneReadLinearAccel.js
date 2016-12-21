793a4d310558424610e8b655169bbf9814c268ad
Álvaro Sánchez Bravo <alvaro.sanchez@bq.com>
2016-12-21 15:53:17 +0100

    phoneReadLinearAccel- Changed return type to float in function declaration

--- src/scripts/bloqs/components/phoneReadLinearAccel.js
+++ src/scripts/bloqs/components/phoneReadLinearAccel.js
@@ -50,7 +50,7 @@ var phoneReadLinearAccel = _.merge(_.clone(OutputBloq, true), {
     code: '{PHONE}.readString()',
     arduino: {
         includes: ['BitbloqSoftwareSerial.h'],
-        extraFunctionCode: 'String getAcceleration(String axis,bqSoftwareSerial & phone){phone.println(String("readLAccel-")+String(axis));String data="";while(data==""){data=phone.readString();}return data.toFloat();}',
+        extraFunctionCode: 'float getAcceleration(String axis,bqSoftwareSerial & phone){phone.println(String("readLAccel-")+String(axis));String data="";while(data==""){data=phone.readString();}return data.toFloat();}',
         code:'getAcceleration({AXIS}, {PHONE})'
     },
     returnType: {
