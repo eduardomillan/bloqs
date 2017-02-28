66b2fea429b320cf1b850c19dbaf1734222c7ad3
Tom Calvo <calvo.tom@gmail.com>
2017-02-28 13:25:29 +0100

    new v2 bloqs and something near bloq

---
+++ src/scripts/bloqs/mbot/somethingNear.js
@@ -0,0 +1,42 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../build-utils'),
+    StatementInputBloq = require('./../statementInputBloq');
+
+/**
+ * Bloq name: mbot-SomethingNear
+ *
+ * Bloq type: Statement-Input
+ *
+ * Description:
+ *
+ * Return type: none
+ */
+
+var bloqMBotSomethingNear = _.merge(_.clone(StatementInputBloq, true), {
+
+    name: 'mbot-somethingnear',
+    bloqClass: 'bloq-mbot-somethingnear',
+    content: [
+        [{
+            alias: 'text',
+            value: 'Si se detecta un obstaculo a poca distancia con '
+        }, {
+            id: 'ULTRASOUND',
+            alias: 'dynamicDropdown',
+            options: 'mkb_ultrasound'
+        }]
+    ],
+    code: '',
+    arduino: {
+        code: 'if({ULTRASOUND}.readDistanceInCM() > 10){{STATEMENTS}}'
+    }
+});
+
+utils.preprocessBloq(bloqMBotSomethingNear);
+bloqMBotSomethingNear.connectors[1].acceptedAliases = ['all', 'ifDown'];
+
+
+module.exports = bloqMBotSomethingNear;
---
+++ src/scripts/bloqs/mbot/stopV2.js
@@ -0,0 +1,40 @@
+/*global require */
+'use strict';
+
+var _ = require('lodash'),
+    utils = require('./../build-utils'),
+    StatementBloq = require('./../statementBloq');
+
+/**
+ * Bloq name: mBotStop
+ *
+ * Bloq type: Statement
+ *
+ * Description: Move the vehicle forward
+ *
+ */
+
+var mBotStop = _.merge(_.clone(StatementBloq, true), {
+
+    name: 'mBotStop-v2',
+    bloqClass: 'bloq-mbot-stop',
+    content: [
+        [{
+            alias: 'text',
+            value: 'bloq-mbot-stop'
+        }]
+    ],
+    code: '',
+    arduino: {
+        includes: ['BitbloqMBot.h'],
+        needInstanceOf: [{
+            name: 'mBot',
+            type: 'BitbloqMBot'
+        }],
+        code: 'mBot.move(1,0);'
+    }
+});
+
+utils.preprocessBloq(mBotStop);
+
+module.exports = mBotStop;
